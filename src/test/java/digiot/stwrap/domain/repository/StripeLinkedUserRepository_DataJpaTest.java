bb301877d51ad9bbcb5cade5a9ed4dac8ebfa321
Digiot <150645838+digiot-abc@users.noreply.github.com>
2023-11-27 04:17:43 +0000

    InitialCommit

--- src/test/java/digiot/stwrap/domain/repository/StripeLinkedUserRepository_DataJpaTest.java
+++ src/test/java/digiot/stwrap/domain/repository/StripeLinkedUserRepository_DataJpaTest.java
@@ -21,21 +21,21 @@ public class StripeLinkedUserRepository_DataJpaTest {
     void testUserIdMapping() {
 
         StripeLinkedUser user = new StripeLinkedUser();
-        user.setUserId(UserId.valueOf("testUser"));
         user.setId(new ULID().nextULID());
+        user.setUserId(UserId.valueOf("testUser"));
         user.setStripeCustomerId("stripeCustomerId");
         user.setDeleted(false);
         user.setIsPrimary(true);
 
         // 保存
-       StripeLinkedUser savedUser = repository.save(user);
+        StripeLinkedUser savedUser = repository.save(user);
 
-       // 検索
-       StripeLinkedUser foundUser = repository.findById(savedUser.getId()).orElseThrow();
-       assertEquals("testUser", foundUser.getUserId().getValue());
+        // 検索
+        StripeLinkedUser foundUser = repository.findById(savedUser.getId()).orElseThrow();
+        assertEquals("testUser", foundUser.getUserId().getValue());
 
-       System.out.println("saved: " + savedUser);
-       System.out.println("found: " + foundUser);
+        System.out.println("saved: " + savedUser);
+        System.out.println("found: " + foundUser);
     }
 }
 
