ac393bd4573c37028be8e4a68543dc91b1f5852a
digiot <digiot.abc@gmail.com>
2023-11-28 05:02:36 +0900

    InitialCommit.

---
+++ src/test/java/digiot/stwrap/domain/repository/StripeSetupIntentRepositoryTest.java
@@ -0,0 +1,56 @@
+package digiot.stwrap.domain.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import com.stripe.exception.StripeException;
+import digiot.stwrap.application.CustomerService;
+import digiot.stwrap.domain.model.StripeSetupIntent;
+import digiot.stwrap.domain.model.UserId;
+import digiot.stwrap.infrastructure.StripeApiKeyInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
+public class StripeSetupIntentRepositoryTest {
+
+  @Autowired
+  CustomerService customerService;
+
+  @Autowired
+  StripeSetupIntentRepository stripeSetupIntentRepository;
+
+  static {
+
+    StripeApiKeyInitializer.initialize();
+  }
+
+  @Test
+  public void testFindByStripeLinkedUserId() throws StripeException {
+
+    // Setup test data
+    String testUserId = "test-user-id";
+    StripeSetupIntent testIntent = new StripeSetupIntent();
+    testIntent.setId("test-setup-intent-id");
+    testIntent.setStripeLinkedUser(customerService.getOrCreateStripeLinkedUser(UserId.valueOf(testUserId))); // Assuming there is a setter for this field
+    stripeSetupIntentRepository.save(testIntent);
+
+    // Retrieve setup intents by Stripe linked user ID
+    List<StripeSetupIntent> retrievedIntents = stripeSetupIntentRepository.findByStripeLinkedUserId(testUserId);
+
+    // Assert the retrieved data
+    assertThat(retrievedIntents).isNotNull();
+    assertThat(retrievedIntents.size()).isEqualTo(1);
+    assertThat(retrievedIntents.get(0).getId()).isEqualTo("test-setup-intent-id");
+  }
+
+  // Additional test cases for other CRUD operations can be added here
+}
--- src/test/resources/initialize.sql
+++ src/test/resources/initialize.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS stripe_linked_user
 (
     id                 VARCHAR(32) PRIMARY KEY,
-    user_id            VARCHAR(255) NOT NULL,
+    user_id            VARCHAR(255),
     stripe_customer_id VARCHAR(255) NOT NULL,
     deleted         BOOLEAN   DEFAULT FALSE,
     created_at         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -11,3 +11,31 @@ CREATE TABLE IF NOT EXISTS stripe_linked_user
 
 CREATE INDEX IF NOT EXISTS idx_stripe_linked_user_user_id ON stripe_linked_user (user_id);
 CREATE INDEX IF NOT EXISTS idx_stripe_linked_user_stripe_customer_id ON stripe_linked_user (stripe_customer_id);
+
+-- stripe_payment_intents
+CREATE TABLE IF NOT EXISTS stripe_payment_intents
+(
+    id VARCHAR(255) PRIMARY KEY,
+    stripe_linked_user_id VARCHAR(32),
+    status VARCHAR(50),
+    amount INT,
+    currency VARCHAR(10),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (stripe_linked_user_id) REFERENCES stripe_linked_user(id) ON DELETE SET NULL
+    );
+
+CREATE INDEX IF NOT EXISTS idx_payment_intents_stripe_linked_user_id ON stripe_payment_intents (stripe_linked_user_id);
+
+-- stripe_setup_intents
+CREATE TABLE IF NOT EXISTS stripe_setup_intents
+(
+    id VARCHAR(255) PRIMARY KEY,
+    stripe_linked_user_id VARCHAR(32),
+    status VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (stripe_linked_user_id) REFERENCES stripe_linked_user(id) ON DELETE SET NULL
+    );
+
+CREATE INDEX IF NOT EXISTS idx_setup_intents_stripe_linked_user_id ON stripe_setup_intents (stripe_linked_user_id);
